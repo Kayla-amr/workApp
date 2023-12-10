45cfd8122084dac719ee3b6a48bd5adc2c1ec42a
Kayla Macias <kaylamacias.rivera@gmail.com>
2023-12-10 16:00:47 -0700

    view equipment

---
+++ .history/client/src/equipment_20231210155922.js
@@ -0,0 +1,34 @@
+let equipments = [];
+
+function getData() {
+    let equipmentPromise = fetch('api/equipment');
+    equipmentPromise.then(response => {
+        return response.json();
+    }).then(data => {
+        equipments = data;
+        viewEquipment(equipments);
+    });
+}
+
+getData();
+
+function viewEquipment(equipments) {
+    let equipmentList = document.querySelector('.equipment');
+    equipmentList.innerHTML = ''; // Clear existing list
+
+    equipments.forEach(equipment => {
+        let checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('id', equipment.id);
+        checkbox.setAttribute('name', equipment.name);
+        checkbox.setAttribute('value', equipment.name);
+        equipmentList.appendChild(checkbox);
+
+        let label = document.createElement('label');
+        label.setAttribute('for', equipment.name);
+        label.innerText = equipment.name;
+        equipmentList.appendChild(label);
+
+    
+    });
+}
